0a627cfb52ce873c1bb982c4e97a420739b8cb0b
Abhishek125pradeep <abhishek333roy@gmail.com>
2023-05-26 12:31:37 +0530

    myjscode

---
+++ js/first.js
@@ -0,0 +1,5 @@
+ let test;    //  declaration 
+test = "123Abhishek";//defination
+console.log(test)
+let test1 = 123544;  //declartion + defination
+console.log(test1)
---
+++ js/thirdway.js
@@ -0,0 +1,5 @@
+function test(){
+    let test = "Java scripit func"
+    document.write("calling finc")
+    console.log(test)
+}
