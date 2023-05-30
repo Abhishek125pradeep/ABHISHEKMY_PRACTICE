ce2a93df1645ade1370a101c41d0d5a2c4af088b
Abhishek125pradeep <abhishek333roy@gmail.com>
2023-05-30 10:12:58 +0530

    angular app

--- myangularpractice/src/app/app.component.ts
+++ myangularpractice/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myangularpractice';
+
+  myname = "Abhishek";
+  lastname = "Nagrale";
+  middlename = "pradeep"
+
 }
---
+++ myangularpractice/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  firstname = "Form Login";
+  test: string = "type_prog"
+  dig: number = 4515121
+  bul: boolean = false
+
+}
