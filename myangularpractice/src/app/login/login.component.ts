5bc4b047f7f885694aec22807b95ffec44e05c62
Abhishek125pradeep <abhishek333roy@gmail.com>
2023-06-05 10:08:31 +0530

    angular practising

--- myangularpractice/src/app/login/login.component.ts
+++ myangularpractice/src/app/login/login.component.ts
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
   firstname = "Form Login";
-  test: string = "type_prog"
-  dig: number = 4515121
-  bul: boolean = false
+  test: string = "type_prog";
+  dig: number = 4515121;
+  bul: boolean = false;
+  panno:any =true
+  panno1:any =[54,645,7,5]
 
 }
